35f459b1e90458d850a0358b6c52a3a6cc5b2416
Adam Steciuk <01143604@pw.edu.pl>
2020-11-15 12:49:39 +0100

    Stworzono prymityw ostrosłupa

---
+++ GKOM_Proj/headers/Pyramid.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "ObjectPrimitive.h"
+
+class Pyramid : public ObjectPrimitive
+{
+	GLfloat size;
+
+public:
+	//x, y, z, transformation
+	Pyramid(GLfloat size, Transformation transMat);
+
+	std::unique_ptr<VertexBuffer> initVertices();
+	std::unique_ptr<IndexBuffer> initIndices();
+};
