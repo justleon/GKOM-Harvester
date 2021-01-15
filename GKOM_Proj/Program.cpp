ac86b8d0a0c025d66a5c9834652e77156e10b8ea
Adam Steciuk <01143604@pw.edu.pl>
2021-01-15 17:55:35 +0100

    texture batch 1

--- GKOM_Proj/Program.cpp
+++ GKOM_Proj/Program.cpp
@@ -125,8 +125,8 @@ int main()
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 		// prepare textures
-		GLuint texture0 = LoadMipmapTexture(GL_TEXTURE0, "textures/container.jpg");
-		GLuint texture1 = LoadMipmapTexture(GL_TEXTURE1, "textures/weiti.png");
+		GLuint texture0 = LoadMipmapTexture(GL_TEXTURE0, "textures/harv_side.png");
+		GLuint texture1 = LoadMipmapTexture(GL_TEXTURE1, "textures/harv_side.png");
 
 		glEnable(GL_DEPTH_TEST);
 
@@ -216,38 +216,22 @@ int main()
 			Pyramid dach(6.50f, t2);
 			dach.draw(shaderProgram);
 
-
 			//wal młyna
 			Transformation t3({ -5.0f, 8.0f, -5.0f },
-				wingSpeed*currentFrame,
+				wingSpeed * currentFrame,
 				{ 0.0f, 0.0f, 1.0f },
 				{ 1.0f, 1.0f, 3.0f });
 			Cylinder wal(0.6f, t3);
 			wal.draw(shaderProgram);
 
 			for (int i = 0; i < numWings; i++) {
-				Transformation t3({ -5.0f, 8.0f, -4.3f + i*0.001f },
-					i*wingAngle + wingSpeed*currentFrame,
+				Transformation t3({ -5.0f, 8.0f, -4.3f + i * 0.001f },
+					i * wingAngle + wingSpeed * currentFrame,
 					{ 0.0f, 0.0f, 1.0f },
 					{ 2.0f, 40.0f, 0.2f });
 				Cube wing(0.3f, t3);
 				wing.draw(shaderProgram);
 			}
-
-			//Transformation transformationMlocarkaPoprzeczka({ posX, heightInMechanism, 0.0f },
-			//	angleInDegrees / 2.0f,
-			//	{ 0.0f, 0.0f, 3.0f },
-			//	{ 0.01f, 0.01f, 1.01f });
-
-			//for (int i = 0; i < numberOfSidesInMechanism; i++)
-			//{
-			//	transformationMlocarkaPoprzeczka.pos[0] = ((posX + 0.8f) * cosf(angle * i)) - (posY * sinf(angle * i)) - 0.8f;
-			//	transformationMlocarkaPoprzeczka.pos[1] = ((posX + 0.8f) * sinf(angle * i)) + (posY * cosf(angle * i));
-			//	transformationMlocarkaPoprzeczka.angle += i * angleInDegrees;
-			//	//element poprzeczny generowany proceduralnie
-			//	Cube mlocarkaElementPoprzeczka(1.0f, transformationMlocarkaPoprzeczka);
-			//	mlocarkaElementPoprzeczka.draw(shaderProgram);
-			//}
 
 			//pod�o�e
 			Transformation trans1({ 0.0f, -0.19f, 0.0f },
